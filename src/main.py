0d043c7ed0cafcddafb457596d1cab20c6204cc3
koz <tik.kozawa@gmail.com>
2026-08-11 23:00:28 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from collectors.tdnet import fetch_tdnet
-from collectors.kev import fetch_kev
+from collectors.kev import fetch_cisa_kev
 
 def main():
     fetch_tdnet()
-    fetch_kev()
+    fetch_cisa_kev()
 
 if __name__ == "__main__":
     main()
