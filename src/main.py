629cac6a61e05ca09f31f8d9ae7c772ee19305f6
koz <tik.kozawa@gmail.com>
2026-08-12 09:11:59 +0900

    main.py を更新

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from collectors.tdnet import fetch_tdnet
 from collectors.cisa_kev import fetch_cisa_kev
+from collectors.ipa import fetch_ipa
 
 def main():
     fetch_tdnet()
     fetch_cisa_kev()
+    fetch_ipa()
 
 if __name__ == "__main__":
     main()
