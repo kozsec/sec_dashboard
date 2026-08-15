7ddb3b24a58eaab45ffd222fefec155a9872b943
koz <tik.kozawa@gmail.com>
2026-08-15 22:39:51 +0900

    Update tdnet.py

--- src/collectors/tdnet.py
+++ src/collectors/tdnet.py
@@ -283,7 +283,7 @@ def save_json(data):
 
     date_str = data[0]["date"].replace("-", "")
     filename = (
-        f"data/security_disclosures_{date_str}.json"
+        f"data/feed_{date_str}.json"
     )
 
     os.makedirs(
