c4b4734202324896e2591052d686dc63901ea47b
koz <tik.kozawa@gmail.com>
2026-08-30 23:34:55 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@ from collectors.tdnet import fetch_tdnet
 from collectors.cisa_kev import fetch_cisa_kev
 from collectors.ipa import fetch_ipa
 from collectors.nco import fetch_nco
-from collectors.ncsc import main as fetch_ncsc
 from collectors.update_epss_cvss import main as update_epss_cvss
+from collectors.get_nist import fetch_nist
+
 
 def main():
     fetch_tdnet()
     fetch_cisa_kev()
     fetch_ipa()
     fetch_nco()
-    fetch_ncsc()
+    fetch_nist()
     update_epss_cvss()
+
 
 if __name__ == "__main__":
     main()
