16c4c8a6ef000f7bfb8effeac44eda2684787481
koz <tik.kozawa@gmail.com>
2026-08-31 08:40:00 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from collectors.tdnet import fetch_tdnet
 from collectors.cisa_kev import fetch_cisa_kev
 from collectors.ipa import fetch_ipa
 from collectors.nco import fetch_nco
-from collectors.get_ncsc import fetch_ncsc
+from collectors.get_ncsc import main as fetch_ncsc
 from collectors.get_nist import fetch_nist
 from collectors.update_epss_cvss import main as update_epss_cvss
 
